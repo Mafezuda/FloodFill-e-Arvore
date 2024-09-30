677a33bb053d3d958b3082bdabb87246b8a9e9e3
Mafezuda <moreira.mafe@gmail.com>
2024-09-30 16:12:25 -0700

    Pilha mais rápida

--- FloodFill/PilhaPontos.java
+++ FloodFill/PilhaPontos.java
@@ -9,7 +9,7 @@ public class PilhaPontos {
 
     public void empilhar(Ponto ponto) {
         if (estaCheia()) {
-            throw new IllegalStateException("A pilha está cheia!");
+            redimensionar();
         }
         this.elementos[++topo] = ponto;
     }
@@ -30,5 +30,12 @@ public class PilhaPontos {
 
     public boolean estaVazia() {
         return topo == -1;
+    }
+
+    // Método para redimensionar a pilha quando necessário
+    private void redimensionar() {
+        Ponto[] novaPilha = new Ponto[elementos.length * 2];
+        System.arraycopy(elementos, 0, novaPilha, 0, elementos.length);
+        this.elementos = novaPilha;
     }
 }
--- FloodFill/ProcessarComPilha.java
+++ FloodFill/ProcessarComPilha.java
@@ -5,33 +5,43 @@ public class ProcessarComPilha {
         Foto foto = new Foto("Fotos/foto.png");
         PainelImagem painel = new PainelImagem(foto, "Visualizando Foto");
         painel.exibirImagem();
-
         Ponto pontoInicial = new Ponto(10, 10);
         int corOriginal = foto.obterCorPonto(pontoInicial);
         int novaCor = new Color(0, 128, 0).getRGB(); // Verde
-
         PilhaPontos pilha = new PilhaPontos(foto.obterAltura() * foto.obterLargura());
         pilha.empilhar(pontoInicial);
 
+        // Contador para controlar a atualização da imagem durante o processamento
+        int contador = 0;
+        int intervaloAtualizacao = 1000;
+
         while (!pilha.estaVazia()) {
-            Ponto atual = pilha.desempilhar(); 
+            Ponto atual = pilha.desempilhar();
             int x = atual.getPosX();
             int y = atual.getPosY();
 
             if (x >= 0 && x < foto.obterLargura() && y >= 0 && y < foto.obterAltura()) {
                 if (foto.obterCorPonto(atual) == corOriginal && !atual.isVerificado()) {
                     foto.alterarCorPonto(atual, novaCor);
+
                     atual.marcarVerificado();
 
-                    pilha.empilhar(new Ponto(x + 1, y));
-                    pilha.empilhar(new Ponto(x - 1, y));
-                    pilha.empilhar(new Ponto(x, y + 1));
-                    pilha.empilhar(new Ponto(x, y - 1));
-
-                    painel.atualizarFoto(foto);
-                    foto.salvarFoto("Fotos/foto_alterada_pilha.png");
+                    if (x + 1 < foto.obterLargura()) pilha.empilhar(new Ponto(x + 1, y));
+                    if (x - 1 >= 0) pilha.empilhar(new Ponto(x - 1, y));
+                    if (y + 1 < foto.obterAltura()) pilha.empilhar(new Ponto(x, y + 1));
+                    if (y - 1 >= 0) pilha.empilhar(new Ponto(x, y - 1));
+                    contador++;
+                    if (contador % intervaloAtualizacao == 0) {
+                        painel.atualizarFoto(foto);
+                    }
                 }
             }
         }
+
+        // Atualiza a imagem uma última vez após o processo
+        painel.atualizarFoto(foto);
+
+        // Salva a imagem alterada no final
+        foto.salvarFoto("Fotos/foto_alterada_pilha.png");
     }
 }
