e79da6788540d3ccc480a020db76560190b8254d
Mafezuda <moreira.mafe@gmail.com>
2024-09-29 17:03:10 -0700

    tentando o print

--- Arvore/Arvore.java
+++ Arvore/Arvore.java
@@ -2,67 +2,92 @@ package Arvore;
 
 public class Arvore {
     private No raiz;
-    private int altura;
-    private int profundidade;
 
-    public Arvore(){ this.raiz = null;}
+    public Arvore() {
+        this.raiz = null;
+    }
 
-    public No getRaiz(){ return raiz;}
+    public No getRaiz() {
+        return raiz;
+    }
 
-    public void setRaiz(No trocar){ this.raiz = trocar;}
+    public void setRaiz(No raiz) {
+        this.raiz = raiz;
+    }
 
-    public void inserir(Letra letra){
-        if( raiz == null){
+    public void inserir(Letra letra) {
+        if (raiz == null) {
             No r = new No(letra);
             setRaiz(r);
             return;
         }
         char[] simbolos = letra.getSimbolos();
-
         No atual = raiz;
 
-        for( int i = 0; i < simbolos.length; i++){
+        for (int i = 0; i < simbolos.length; i++) {
             char simbolo = letra.verifica(i);
-            
-            if( simbolo == '.' ){
-                if( atual.getEsquerda() == null){
+            if (simbolo == '.') {
+                if (atual.getEsquerda() == null) {
                     atual.setEsquerda(new No(letra));
                     break;
-                }
-                else{
+                } else {
                     atual = atual.getEsquerda();
-                } 
-            }
-            else{
-                if( atual.getDireita() == null){
+                }
+            } else {
+                if (atual.getDireita() == null) {
                     atual.setDireita(new No(letra));
                     break;
-                }
-                else{
-                    atual =  atual.getDireita();
+                } else {
+                    atual = atual.getDireita();
                 }
             }
         }
     }
 
-    public No pesquisar(char[] simbolos){
+    public No pesquisar(char[] simbolos) {
         No atual = raiz;
 
-        for (int i = 0; i < simbolos.length; i++) {
-            char simbolo = simbolos[i];
-
-            if( simbolo != ' '){
-                if( simbolo == '.' ){
-                    atual.getEsquerda();
-                
-                }
-                else{
-                    atual.getDireita();
-                }
-
+        for (char simbolo : simbolos) {
+            if (simbolo == '.') {
+                atual = atual.getEsquerda();
+            } else if (simbolo == '-') {
+                atual = atual.getDireita();
             }
         }
         return atual;
     }
 
+    public void imprimirArvore() {
+        int h = altura(raiz);
+        for (int i = 0; i < h; i++) {
+            imprimirNivel(raiz, i, h);
+            System.out.println();
+        }
+    }
+
+    private void imprimirNivel(No no, int nivel, int altura) {
+        if (no == null) {
+            for (int j = 0; j < Math.pow(2, altura - nivel) - 1; j++) {
+                System.out.print("   "); // Espaço para manter o alinhamento
+            }
+            return;
+        }
+
+        if (nivel == 0) {
+            System.out.print(no.getLetra().getValor() + " ");
+        } else {
+            imprimirNivel(no.getEsquerda(), nivel - 1, altura);  // Imprime a subárvore esquerda primeiro
+            for (int j = 0; j < Math.pow(2, altura - nivel) - 1; j++) {
+                System.out.print("   "); // Espaço para manter o alinhamento
+            }
+            imprimirNivel(no.getDireita(), nivel - 1, altura);  // Imprime a subárvore direita
+        }
+    }
+
+    private int altura(No no) {
+        if (no == null) {
+            return 0;
+        }
+        return 1 + Math.max(altura(no.getEsquerda()), altura(no.getDireita()));
+    }
 }
--- Arvore/Main.java
+++ Arvore/Main.java
@@ -4,132 +4,123 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void inserirElementos(Arvore a){
-        Letra[] letras = new Letra[26];
+    public static void inserirElementos(Arvore a) {
         a.inserir(new Letra(' '));
 
-        char[] e = {'.'};
+        char[] e = { '.' };
         a.inserir(new Letra('E', e));
 
-        char[] i = {'.', '.'};
+        char[] i = { '.', '.' };
         a.inserir(new Letra('I', i));
 
-        char[] s = {'.', '.', '.'};
+        char[] s = { '.', '.', '.' };
         a.inserir(new Letra('S', s));
 
-        char[] h = {'.', '.', '.', '.'};
+        char[] h = { '.', '.', '.', '.' };
         a.inserir(new Letra('H', h));
 
-        char[] v = {'.', '.', '.', '-'};
+        char[] v = { '.', '.', '-', '.' };
         a.inserir(new Letra('V', v));
 
-        char[] u = {'.', '.', '-'};
+        char[] u = { '.', '.', '-' };
         a.inserir(new Letra('U', u));
 
-        char[] f = {'.', '.', '-', '.'};
+        char[] f = { '.', '.', '-', '.' };
         a.inserir(new Letra('F', f));
 
-        char[] A = {'.', '-'};
+        char[] A = { '.', '-' };
         a.inserir(new Letra('A', A));
 
-        char[] r = {'.', '-', '.'};
+        char[] r = { '.', '-', '.' };
         a.inserir(new Letra('R', r));
 
-        char[] l = {'.', '-', '.', '.'};
+        char[] l = { '.', '-', '.', '.' };
         a.inserir(new Letra('L', l));
 
-        char[] w = {'.', '-', '-'};
+        char[] w = { '.', '-', '-' };
         a.inserir(new Letra('W', w));
 
-        char[] p = {'.', '-', '-', '.'};
+        char[] p = { '.', '-', '-', '.' };
         a.inserir(new Letra('P', p));
 
-        char[] j = {'.', '-', '-', '-'};
+        char[] j = { '.', '-', '-', '-' };
         a.inserir(new Letra('J', j));
 
-        char[] t = {'-'};
+        char[] t = { '-' };
         a.inserir(new Letra('T', t));
 
-        char[] m = {'-', '-'};
+        char[] m = { '-', '-' };
         a.inserir(new Letra('M', m));
 
-        char[] o = {'-', '-', '-'};
-        a.inserir(new Letra('o', o));
+        char[] o = { '-', '-', '-' };
+        a.inserir(new Letra('O', o));
 
-        char[] g = {'-', '-', '.'};
+        char[] g = { '-', '-', '.' };
         a.inserir(new Letra('G', g));
 
-        char[] q = {'-', '-', '.', '-'};
+        char[] q = { '-', '-', '.', '-' };
         a.inserir(new Letra('Q', q));
 
-        char[] z = {'-', '-', '.', '.'};
+        char[] z = { '-', '-', '.', '.' };
         a.inserir(new Letra('Z', z));
 
-        char[] n = {'-', '.'};
+        char[] n = { '-', '.' };
         a.inserir(new Letra('N', n));
 
-        char[] k = {'-', '.', '-'};
+        char[] k = { '-', '.', '-' };
         a.inserir(new Letra('K', k));
 
-        char[] y = {'-', '.', '-', '-'};
+        char[] y = { '-', '.', '-', '-' };
         a.inserir(new Letra('Y', y));
 
-        char[] c = {'-', '.', '-', '.'};
+        char[] c = { '-', '.', '-', '.' };
         a.inserir(new Letra('C', c));
 
-        char[] d = {'-', '.', '.'};
+        char[] d = { '-', '.', '.' };
         a.inserir(new Letra('D', d));
 
-        char[] b = {'-', '.', '.', '.'};
+        char[] b = { '-', '.', '.', '.' };
         a.inserir(new Letra('B', b));
 
-        char[] x = {'-', '.', '.', '-'};
+        char[] x = { '-', '.', '.', '-' };
         a.inserir(new Letra('X', x));
     }
-    
-        
+
     public static void main(String[] args) {
         Arvore a = new Arvore();
         inserirElementos(a);
-      
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Escolha uma opção: ");
-        System.out.println("1- Digitar uma código");
+        System.out.println("1- Digitar um código");
         System.out.println("2- Imprimir árvore");
 
         int opcao = scan.nextInt();
+        scan.nextLine(); // Consumir nova linha
 
         switch (opcao) {
             case 1:
-                Scanner s = new Scanner(System.in);
-                System.out.println("Digite o código(utilize espaço para separar as letras):");     
-                String codigo = s.nextLine();
-                char[] simbolos = new char[4];
-                char[] decifrado = new char[codigo.length()];
-                int posicao = 0;
-                for (int i = 0; i < codigo.length(); i++) {
-                    if(codigo.charAt(i) == ' '){
-                       No no = a.pesquisar(simbolos);
-                       decifrado[posicao] = no.getLetra().getValor();
-                       posicao++;
-                       simbolos = new char[4];
-                       i = -1;
-                    }
-                    else{
-                        char letra = codigo.charAt(i); 
-                        simbolos[i] = letra;
+                System.out.println("Digite o código (utilize espaço para separar as letras):");
+                String codigo = scan.nextLine();
+                String[] simbolos = codigo.split(" ");
+                StringBuilder decifrado = new StringBuilder();
+
+                for (String simbolo : simbolos) {
+                    No no = a.pesquisar(simbolo.toCharArray());
+                    if (no != null) {
+                        decifrado.append(no.getLetra().getValor());
                     }
                 }
+
+                System.out.println("Decifrado: " + decifrado.toString());
                 break;
             case 2:
+                a.imprimirArvore(); // Chama o método para imprimir a árvore
                 break;
             default:
-                System.out.println("Opção inválida. Tente novamente.");;
+                System.out.println("Opção inválida. Tente novamente.");
         }
-
-
-
+        scan.close();
     }
-    
 }
